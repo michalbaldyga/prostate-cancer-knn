c5b2d84b33d11f0cbe1f94da779d0bae12fd8528
michalbaldyga <michal.baldyga01@gmail.com>
2023-02-15 20:03:55 +0100

    Data processing

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello Git")
+import pandas as pd
+from data_processing import parse_data, rescale
+
+
+if __name__ == '__main__':
+
+    # Reading data
+    raw_data = pd.read_csv('Prostate_Cancer.csv')
+
+    # Data processing
+    clean_data = parse_data(raw_data)
+
+    # Data standardization
+    rescaled_data = rescale(clean_data)
