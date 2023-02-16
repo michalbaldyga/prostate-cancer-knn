2246b4a00c9ba57bef3cecbea6966550217a4e53
michalbaldyga <michal.baldyga01@gmail.com>
2023-02-16 11:19:51 +0100

    Test model

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
-from data_processing import parse_data, rescale
+from data_processing import parse_data, rescale, split_data
+from constants import SPLIT_RATIO, K_NEAREST_NEIGHBOR
+from classifier import knn
+from collections import defaultdict
 
 
 if __name__ == '__main__':
@@ -12,3 +15,22 @@ if __name__ == '__main__':
 
     # Data standardization
     rescaled_data = rescale(clean_data)
+
+    # Splitting data
+    train_set, test_set = split_data(rescaled_data, SPLIT_RATIO)
+
+    # Classification
+    wrong_predicts = 0
+    confusion_matrix = defaultdict(int)
+
+    for patient in test_set:
+        predicted = knn(K_NEAREST_NEIGHBOR, train_set, patient.point)
+        actual = patient.label
+        if actual != predicted:
+            wrong_predicts += 1
+
+        confusion_matrix[(predicted, actual)] += 1
+
+    # Results
+    print("Accuracy: " + str((len(test_set) - wrong_predicts) / len(test_set)))
+    print(confusion_matrix)
