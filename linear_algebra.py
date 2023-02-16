37ca6a1588cf5ce1405114dc5f462953507baa01
michalbaldyga <michal.baldyga01@gmail.com>
2023-02-16 10:05:45 +0100

    Add function to calculate vectors stdevs

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,5 +1,5 @@
 from typing import List
-import math
+from statistics import standard_deviation
 
 Vector = List[float]
 
@@ -12,3 +12,8 @@ def vector_mean(vectors: List[Vector]) -> Vector:
 def sum_of_squares(v: Vector) -> float:
     """v_1 * v_1 + ... + v_n * v_n"""
     return sum([v_i ** 2 for v_i in v])
+
+
+def vector_stdev(vectors: List[Vector]) -> Vector:
+    """Calculates vectors stdevs."""
+    return [standard_deviation([vector[i] for vector in vectors]) for i in range(len(vectors[0]))]
