4fb0e3be070794520a037376428cc07b54b373f7
michalbaldyga <michal.baldyga01@gmail.com>
2023-02-16 10:06:17 +0100

    Refactor scale and rescale functions

--- data_processing.py
+++ data_processing.py
@@ -2,11 +2,11 @@ import pandas as pd
 from Patient import Patient
 from typing import List, Tuple
 from constants import FEATURES_NO
-from linear_algebra import Vector, vector_mean
-from statistics import standard_deviation
+from linear_algebra import Vector, vector_mean, vector_stdev
 
 
 def parse_data(data: pd.DataFrame) -> List[Patient]:
+    """Converts raw data to format: List[Patient]"""
     elements = []
     for i in range(len(data)):
         point = [data.iloc[i][j] for j in range(2, FEATURES_NO)]
@@ -16,28 +16,27 @@ def parse_data(data: pd.DataFrame) -> List[Patient]:
 
 
 def scale(data: List[Vector]) -> Tuple[Vector, Vector]:
-    dim = len(data[0])
-    means = vector_mean(data)
-    stdevs = [standard_deviation([vector[i] for vector in data])
-              for i in range(dim)]
-    return means, stdevs
+    """Returns means and standard deviations of each column."""
+    return vector_mean(data), vector_stdev(data)
 
 
 def rescale(data: List[Patient]) -> List[Patient]:
+    """Rescales the input so that each column has a mean of 0 and a standard deviation of 1."""
+
+    # Extracting points from list of patients
     points = [patient.point for patient in data]
 
-    dim = len(points[0])
-    means, stdevs = scale(points)
-
+    # Making copy of each vector
     rescaled = [v.copy() for v in points]
 
+    # Rescaling if necessary
+    means, stdevs = scale(points)
     for v in rescaled:
-        for i in range(dim):
+        for i in range(len(points[0])):
             if stdevs[i] > 0:
                 v[i] = (v[i] - means[i]) / stdevs[i]
 
-    rescaled_data = []
-    for i in range(len(data)):
-        rescaled_data.append(Patient(rescaled[i], data[i].label))
+    # Joining points with labels
+    rescaled_data = [Patient(rescaled[i], data[i].label) for i in range(len(data))]
 
     return rescaled_data
