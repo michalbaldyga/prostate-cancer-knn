80d769bec92e15a1b7db7732f168fab546446bb0
michalbaldyga <michal.baldyga01@gmail.com>
2023-02-16 10:05:03 +0100

    Add comments and Vector type

--- statistics.py
+++ statistics.py
@@ -1,14 +1,19 @@
-from linear_algebra import Vector
 from math import sqrt
+from typing import List
+
+Vector = List[float]
 
 
 def mean(v: Vector) -> float:
+    """Calculates mean of vector."""
     return sum(v) / len(v)
 
 
 def variance(v: Vector) -> float:
+    """Calculates variance of vector."""
     return sum((v_i - mean(v)) ** 2 for v_i in v) / (len(v) - 1)
 
 
 def standard_deviation(v: Vector) -> float:
+    """Calculates standard deviation of vector."""
     return sqrt(variance(v))
